5e0d3a24defe130b22150c9527e31a083e292559
Kitwana Akil <kakil@me.com>
2023-05-19 14:05:55 -0400

    Initial Commit

---
+++ Adigi Hologram/Adigi_HologramApp.swift
@@ -0,0 +1,17 @@
+//
+//  Adigi_HologramApp.swift
+//  Adigi Hologram
+//
+//  Created by Kitwana Akil on 5/19/23.
+//
+
+import SwiftUI
+
+@main
+struct Adigi_HologramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
